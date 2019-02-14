2a99802c4eb8e15b812555c550923b1bffae3d02
Julian Stecklina <js@alien8.de>
2019-02-14 22:57:00 +0100

    Add callbacks in the memory testing class
    Properly testing whether atomic operations are used correctly is hard.
    To do this for the page table walker, introduce callbacks that can be
    used to modify the memory after a memory operation.
    
    The intended usecase is to "swap" page table contents when A/D bit
    updates failed and test whether this still results in a correct
    translation.

--- test/memory.hpp
+++ test/memory.hpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <cstdint>
 #include <forward_list>
+#include <functional>
+#include <map>
+#include <tuple>
 
 #include "utilities.hpp"
 
@@ -23,41 +26,92 @@ struct accessed_uninitialized_memory {
 // operations were used.
 template <typename WORD>
 class memory {
+public:
+  enum class operation_type { READ, WRITE };
 
-  enum class opcode { READ, WRITE };
+private:
 
-  struct operation {
-    opcode opc;
+  using this_t = memory<WORD>;
+
+  struct operation_head {
+    operation_type opc;
     uint64_t address;
+
+    // We need a comparison operator, because we use this struct in std::map
+    // below.
+    bool operator<(operation_head const &rhs) const
+    {
+      return opc < rhs.opc or (opc == rhs.opc and address < rhs.address);
+    }
+
+    operation_head() = delete;
+    operation_head(operation_type opc_, uint64_t address_)
+      : opc(opc_), address(address_)
+    {}
+  };
+
+  struct operation : private operation_head {
     WORD value;
 
     operation() = delete;
 
     static operation write(uint64_t address, WORD value)
     {
-      return operation(opcode::WRITE, address, value);
+      return operation(operation_type::WRITE, address, value);
     }
 
     static operation read(uint64_t address, WORD value)
     {
-      return operation(opcode::WRITE, address, value);
+      return operation(operation_type::WRITE, address, value);
     }
 
     bool matches_read(uint64_t address_) const
     {
-      return opc == opcode::WRITE and address == address_;
+      return this->opc == operation_type::WRITE and this->address == address_;
     }
 
   private:
 
-    operation(opcode opc_, uint64_t address_, WORD value_)
-      : opc(opc_), address(address_), value(value_)
+    operation(operation_type opc_, uint64_t address_, WORD value_)
+      : operation_head(opc_, address_), value(value_)
     {}
   };
 
   // Log of all operations. From new to old, i.e. new operations are added to
   // the front.
   std::forward_list<operation> history;
+
+  using async_handler_fn = std::function<void(this_t *)>;
+
+  // Remember all async handlers here.
+  std::map<operation_head, async_handler_fn> async_handlers;
+
+  void maybe_execute_async_handler(operation_head const &op)
+  {
+    auto node = async_handlers.extract(op);
+
+    if (not node)
+      return;
+
+    node.mapped()(this);
+  }
+
+  // Will execute a matching async handler on destruction. Can be used as a
+  // scope guard.
+  struct async_handler_guard {
+    this_t *memory;
+    operation_head op;
+
+    async_handler_guard() = delete;
+    async_handler_guard(this_t *memory_, operation_type type_, uint64_t address_)
+      : memory(memory_), op(type_, address_)
+    {}
+
+    ~async_handler_guard()
+    {
+      memory->maybe_execute_async_handler(op);
+    }
+  };
 
   bool is_naturally_aligned(uint64_t address)
   {
@@ -69,6 +123,7 @@ public:
   void write(uint64_t address, WORD value)
   {
     fast_assert(is_naturally_aligned(address));
+    async_handler_guard g {this, operation_type::WRITE, address};
 
     history.emplace_front(operation::write(address, value));
   }
@@ -76,6 +131,7 @@ public:
   WORD read(uint64_t address)
   {
     fast_assert(is_naturally_aligned(address));
+    async_handler_guard g {this, operation_type::READ, address};
 
     auto it = std::find_if(history.begin(), history.end(),
                            [address] (auto const &op) { return op.matches_read(address); });
@@ -86,5 +142,16 @@ public:
       history.emplace_front(operation::read(address, it->value));
       return it->value;
     }
+  }
+
+  // Schedule a function to be called once after a specified memory operation.
+  //
+  // This can be used to modify memory at the right time to make cmpxchg
+  // operations fail and increase test coverage of otherwise hard to reach code
+  // paths.
+  template <typename H>
+  void execute_after(operation_type op_type, uint64_t address, H &&async_handler)
+  {
+    async_handlers.emplace(operation_head {op_type, address}, std::forward<H>(async_handler));
   }
 };
--- test/test_memory.cpp
+++ test/test_memory.cpp
@@ -1,3 +1,6 @@
+// This is a test for the memory class that we use as the memory backend for the
+// page table walker tests.
+
 #include <catch2/catch.hpp>
 #include "memory.hpp"
 
@@ -22,5 +25,57 @@ TEST_CASE("Memory can read and write data", "[memory]")
     mem.write(0, 456);
 
     REQUIRE(mem.read(0) == 456);
+  }
+}
+
+// Return a function that sets a boolean value when it's called.
+static auto bool_setter(bool *b)
+{
+  return [b] (...) { *b = true; };
+}
+
+TEST_CASE("Asynchronous handlers are called")
+{
+  memory<uint32_t> mem;
+
+  SECTION("A handler is called for the correct operation") {
+    bool called = false;
+
+    mem.execute_after(memory<uint32_t>::operation_type::WRITE, 4, bool_setter(&called));
+
+    mem.write(4, 0);
+  }
+
+  SECTION("A handler is not called for the wrong address or operation") {
+    bool called = false;
+
+    mem.execute_after(memory<uint32_t>::operation_type::READ, 4, bool_setter(&called));
+
+    mem.write(0, 0);
+    mem.write(4, 0);
+
+    CHECK_FALSE(called);
+  }
+
+  SECTION("A handler is called after the operation it matched on") {
+    uint32_t read_value = ~0;
+
+    mem.write(0, 1);
+    mem.execute_after(memory<uint32_t>::operation_type::WRITE, 0,
+                             [&read_value] (memory<uint32_t> *m) { read_value = m->read(0); });
+    mem.write(0, 2);
+
+    REQUIRE(read_value == 2);
+  }
+
+  SECTION("A handler is called only once") {
+    int count = 0;
+
+    mem.execute_after(memory<uint32_t>::operation_type::WRITE, 0, [&count] (...) { count++; });
+
+    mem.write(0, 1);
+    mem.write(0, 1);
+
+    REQUIRE(count == 1);
   }
 }
